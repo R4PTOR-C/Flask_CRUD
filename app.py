42c0a815b70a769f26a74f478c13c5a10a91d33c
rafael.caldas <rafaelcroriz7@gmail.com>
2023-07-12 09:36:11 -0300

    Adicionadoopção de editar e deletar usuarios

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from config import DevelopmentConfig, ProductionConfig
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
 
 
@@ -40,6 +40,49 @@ def adicionar_usuario():
         return 'Usuário adicionado com sucesso!'
     else:
         return render_template('new.html')
+
+@app.route('/deletar_usuario/<int:id>', methods=['POST'])
+def deletar_usuario(id):
+    # Lógica para deletar o usuário com o ID fornecido
+
+    conn = psycopg2.connect(host='localhost', user='rafael', password='nova_senha', database='banco_de_dados')
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM usuarios WHERE id = %s', (id,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return redirect(url_for('show_table'))
+
+
+@app.route('/editar_usuario/<int:id>', methods=['GET'])
+def editar_usuario(id):
+    # Lógica para buscar os dados do usuário com o ID fornecido
+    conn = psycopg2.connect(host='localhost', user='rafael', password='nova_senha', database='banco_de_dados')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM usuarios WHERE id = %s', (id,))
+    usuario = cursor.fetchone()
+    cursor.close()
+    conn.close()
+
+    # Renderizar o template HTML do formulário de edição e passar os dados do usuário
+    return render_template('edit.html', usuario=usuario)
+
+
+@app.route('/atualizar_usuario/<int:id>', methods=['POST'])
+def atualizar_usuario(id):
+    # Lógica para atualizar os dados do usuário com o ID fornecido
+    nome = request.form.get('nome')
+    email = request.form.get('email')
+    idade = request.form.get('idade')
+
+    conn = psycopg2.connect(host='localhost', user='rafael', password='nova_senha', database='banco_de_dados')
+    cursor = conn.cursor()
+    cursor.execute('UPDATE usuarios SET nome = %s, email = %s, idade = %s WHERE id = %s', (nome, email, idade, id))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return redirect(url_for('show_table'))
 
 
 if __name__ == '__main__':
