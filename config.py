fdfaa511cf7cbb305cad75f8043fdfbda2e7d58f
rafael.caldas <rafaelcroriz7@gmail.com>
2023-08-10 10:32:01 -0300

    .

--- config.py
+++ config.py
@@ -7,14 +7,14 @@ class Config(object):
 class DevelopmentConfig(Config):
     DEBUG = True
     # Configurações específicas para ambiente de desenvolvimento
-    DATABASE_HOST = 'localhost'  # Set the development database host here
+    DATABASE_HOST = 'localhost'
     DATABASE_USER = 'rafael'
     DATABASE_PASSWORD = 'nova_senha'
     DATABASE_NAME = 'banco_de_dados'
 class ProductionConfig(Config):
     DEBUG = False
     # Configurações específicas para ambiente de produção
-    DATABASE_HOST = 'dpg-ciqkhe6nqql4qa7csi10-a.oregon-postgres.render.com'  # Set the production database host here
+    DATABASE_HOST = 'dpg-ciqkhe6nqql4qa7csi10-a.oregon-postgres.render.com'
     DATABASE_USER = 'rafael'
     DATABASE_PASSWORD = 'PuObkrabERkdbxhSFmGtAuXTum4fTjr5'
     DATABASE_NAME = 'banco_de_dados_rvj4'
